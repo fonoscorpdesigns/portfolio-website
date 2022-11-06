3deb9ec021cf284c255ab437a69407cd08a757b7
fonoscorpdesigns <fonoscorpdesigns@gmail.com>
2022-11-06 18:41:13 +0100

    trigger toggle menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!!")
+
+function menuToggle() {
+    'use strict';
+    var m = document.getElementById('myNavtoggle');
+   if (m.className === 'navtoggle') {
+    m.className += ' responsive';
+  } else {
+    m.className = 'navtoggle';
+  }
+}
